db1244e71799a9fbf0f888dbb9b366b93ec0b6be
Santiago Paul H. <69270377+santiagopaulh@users.noreply.github.com>
2026-07-19 01:55:52 -0500

    Update social media links in Data component

--- src/components/Utilities/Data/Data.tsx
+++ src/components/Utilities/Data/Data.tsx
@@ -3,9 +3,9 @@ export type AvailableLanguages = "en" | "es"
 export const Data = {
     mySocialLinks: {
         whatsapp: "https://wa.me/+593995519509",
-        instagram: "https://instagram.com/santi.herreram",
-        github: "https://github.com/santipaulh",
+        instagram: "https://instagram.com/santiagopaulh",
+        github: "https://github.com/santiagopaulh",
         facebook: "https://facebook.com/",
-        linkedin: "https://linkedin.com/in/santipaulh"
+        linkedin: "https://linkedin.com/in/santiagopaulh"
     }
 }
